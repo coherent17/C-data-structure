07c1d294ec590cfc052292ef9cf3f8181ea5dc43
coherent17 <mnb51817@gmail.com>
2021-08-06 22:44:40 +0800

    linkedlist to array

--- Linked List/linkedlist_functional-module.c
+++ Linked List/linkedlist_functional-module.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node{
+struct nodes{
     int value;
-    struct node *next;
+    struct nodes *next;
 };
-typedef struct node node_t;
+//avoid typing struct many times
+typedef struct nodes node;
 
-void printlist(node_t *head){
-    node_t *temp=head;
+void printlist(node *head){
+    node *temp=head;
     while(temp!=NULL){
         printf("%d - ", temp->value);
         temp=temp->next;
@@ -16,27 +17,27 @@ void printlist(node_t *head){
     printf("\n");
 }
 
-node_t *create_new_node(int value){
-    node_t *result=malloc(sizeof(node_t));
+node *create_new_node(int value){
+    node *result=malloc(sizeof(node));
     result->value=value;
     //temporary point to NULL, we'll point to the exact address in the main function
     result->next=NULL;
     return result;
 }
 
-node_t *insert_at_head(node_t *head, node_t *node_to_insert){
+node *insert_at_head(node *head, node *node_to_insert){
     node_to_insert->next=head;
     //return the new head address
     return node_to_insert;
 }
 
-void insert_after_node(node_t *node_to_insert_after, node_t *newnode){
+void insert_after_node(node *node_to_insert_after, node *newnode){
     newnode->next=node_to_insert_after->next;
     node_to_insert_after->next=newnode;
 }
 
-void delete_node(node_t *head, int value){
-    node_t *temp=head;
+void delete_node(node *head, int value){
+    node *temp=head;
     while(temp->next!=NULL){
         if(temp->next->value==value){
             temp->next=temp->next->next;
@@ -45,8 +46,8 @@ void delete_node(node_t *head, int value){
     }
 }
 
-node_t *find_node(node_t *head,int value){
-    node_t *temp=head;
+node *find_node(node *head,int value){
+    node *temp=head;
     while(temp!=NULL){
         if(temp->value==value) return temp;
         //move to the next address in the linkedlist
@@ -56,8 +57,8 @@ node_t *find_node(node_t *head,int value){
     return NULL;
 }
 
-int how_many_node(node_t *head){
-    node_t *temp=head;
+int how_many_node(node *head){
+    node *temp=head;
     int count=0;
     while(temp!=NULL){
         count+=1;
@@ -67,8 +68,8 @@ int how_many_node(node_t *head){
 }
 
 //using bubble_sort to sort the linkedlist
-void sort_linkedlist(node_t *head){
-    node_t *i,*j;
+void sort_linkedlist(node *head){
+    node *i,*j;
     for(i=head;i->next!=NULL;i=i->next){
         for(j=i->next;j!=NULL;j=j->next){
             if(i->value>j->value){
@@ -82,9 +83,9 @@ void sort_linkedlist(node_t *head){
 
 //want to find the mid point of the linkedlist and return the address
 //using two pointer method: fast move two and slow move one
-node_t *middle_of_the_linkedlist(node_t *head){
-    node_t *slow_pointer=head;
-    node_t *fast_pointer=head->next;
+node *middle_of_the_linkedlist(node *head){
+    node *slow_pointer=head;
+    node *fast_pointer=head->next;
     //slow, fast
     while(fast_pointer!=NULL){
         //fast move one step ahead  slow, ___, fast
@@ -100,25 +101,22 @@ node_t *middle_of_the_linkedlist(node_t *head){
     return slow_pointer;
 }
 
-//using do-while to loop the binary search do it first then check
-node_t *binary_search_node(node_t *head, int number){
-    node_t *left=head;
-    node_t *right=NULL;
-    do{
-        node_t *mid=middle_of_the_linkedlist(head);
-        //if middle element is same as the required number
-        if(mid->value==number) return mid;
-        //middle element is greater than the element
-        else if(mid->value<number) left=mid->next;
-        else right=mid;
-    }while(right==NULL || right!=left);
-    //if not found the value, return NULL
-    return NULL;
+int *copy_linkedlist_to_array(node *head){
+    node *temp=head;
+    int length=how_many_node(head);
+    int *array=malloc(sizeof(int)*length);
+    int index=0;
+    while(temp!=NULL){
+        array[index]=temp->value;
+        index+=1;
+        temp=temp->next;
+    }
+    return array;
 }
 
 int main(){
-    node_t *head=NULL;
-    node_t *temp;
+    node *head=NULL;
+    node *temp;
 
     for(int i=0;i<25;i++){
         temp=create_new_node(i);
@@ -144,8 +142,9 @@ int main(){
     temp=middle_of_the_linkedlist(head);
     printf("the middle elemennt in the linkedlist is: %d\n", temp->value);
 
-    //binary search:
-    temp=binary_search_node(head,19);
-    printf("%d\n", temp->value);
+    int *array=copy_linkedlist_to_array(head);
+    for(int i=0;i<node_count;i++){
+        printf("%d ",array[i]);
+    }
     return 0;
 }
