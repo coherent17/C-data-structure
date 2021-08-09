a770c1c3116fb67f4803634bf92008fb6c97173d
coherent17 <mnb51817@gmail.com>
2021-08-09 23:03:06 +0800

    finish the basic structure

--- Linked List/hospital_patient_data_record.c
+++ Linked List/hospital_patient_data_record.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct data{
     char name[15];
@@ -11,7 +12,10 @@ struct data{
 
 typedef struct data person;
 
-person *create_person(person *head){
+//global variable
+person *head=NULL;
+
+person *create_person(){
     //personal data
     person *new_person=malloc(sizeof(person));
     printf("Please enter the user name\n");
@@ -60,7 +64,126 @@ void insert_at_tail(person **head, person *person_to_insert){
     person_to_insert->next=NULL;
 }
 
+void insert_after_someone(person **head, person *person_to_insert){
+    printf("The person's name to insert after:");
+    printf("\n");
+    char name[15];
+    scanf("%s", name);
+    person *temp=*head;
+    while(temp!=NULL){
+        if(strcmp(temp->name, name)==0){
+            person_to_insert->next=temp->next;
+            temp->next=person_to_insert;
+            printf("Insert sucessfully!\n");
+        }
+        temp=temp->next;
+    }
+    printf("Can't found the person named %s\n",name);
+}
+
+void insert_personal_data(){
+    if(head==NULL){
+        printf("\nPlease enter 1 to create data first!\n\n");
+        return;
+    } 
+    printf("Please enter the person data to insert:\n");
+    person *temp;
+    temp=create_person();
+    printf("\nPlease enter the option to insert\n");
+    printf("1. insert at head\t2. insert at tail\t3.insert after someone\n");
+    int option;
+    scanf("%d",&option);
+    if(option==1){
+        insert_at_head(&head,temp);
+    }
+    else if(option==2){
+        insert_at_tail(&head,temp);
+    }
+    else{
+        insert_after_someone(&head,temp);
+    }
+}
+
+void delete_person_data(person **head){
+    if(*head==NULL){
+        printf("\nPlease enter 1 to create data first!\n\n");
+        return;
+    } 
+    printf("Please enter the name of the person to delete:\n");
+    char name[15];
+    scanf("%s",name);
+    person *temp=*head;
+    //if the delete person is the head person:
+    if(strcmp(temp->name,name)==0 && temp==*head){
+        *head=temp->next;
+        printf("Deleted %s in list(head) successfully!\n",name);
+        return;
+    }
+    
+    temp=*head;
+    while(temp!=NULL){
+        if(strcmp(temp->next->name,name)==0){
+            temp->next=temp->next->next;
+            printf("Deleted %s in list successfully!\n",name);
+            return;
+        }
+        temp=temp->next;
+    }
+    printf("Not found %s in the list\n",name);
+}
+
+void person_lookup(person *head){
+    if(head==NULL){
+        printf("\nPlease enter 1 to create data first!\n\n");
+        return;
+    } 
+    printf("Please enter the name of the person to lookup the data:\n");
+    char name[15];
+    scanf("%s",name);
+    person *temp=head;
+    while(temp!=NULL){
+        if(strcmp(temp->name,name)==0){
+            print_personal_data(temp);
+            return;
+        }
+        temp=temp->next;
+    }
+    printf("\nNot found %s in the list!\n\n",name);
+}
+
+void modify_person(person *head){
+    if(head==NULL){
+        printf("\nPlease enter 1 to create data first!\n\n");
+        return;
+    }
+    printf("Please enter the name of the person to modify:\n");
+    char name[15];
+    scanf("%s",name);
+    person *temp=head;
+    while(temp!=NULL){
+        if(strcmp(temp->name,name)==0){
+            printf("Please enter the new user name\n");
+            scanf("%s",temp->name);
+            printf("Please enter the new age of the user\n");
+            scanf("%d",&(temp->age));
+            printf("Please enter the new height of the user\n");
+            scanf("%f",&(temp->height));
+            printf("Please enter the new weight of the user\n");
+            scanf("%f",&(temp->weight));
+            printf("Finish modifying, here is the new personal data:\n");
+            print_personal_data(temp);
+            return;
+        }
+        temp=temp->next;
+    }
+    printf("Not found %s in the list\n\n",name);
+}
+
 void printList(person *head){
+    if(head==NULL){
+        printf("\nPlease enter 1 to create data first!\n\n");
+        return;
+    } 
     person *temp=head;
     int count=1;
     while(temp!=NULL){
@@ -73,28 +196,54 @@ void printList(person *head){
     printf("\n");
 }
 
+void menu(){
+    printf("********************************************\n");
+    printf("        Hospital personal datalist          \n");
+    printf("                                            \n");
+    printf("\t1. create personal data\n");
+    printf("\t2. print the datalist\n");
+    printf("\t3. insert the data\n");
+    printf("\t4. delete the data\n");
+    printf("\t5. look up personal data\n");
+    printf("\t6. modify the personal data\n");
+    printf("********************************************\n");
+    printf("Please enter your choice(1~5, Ctrl+z for exit):");
+}
 
+void selection_function(){
+    menu();
+    int choice;
+    while(scanf("%d", &choice)!=EOF){
+        if(choice==1){
+            person *temp=create_person(head);
+            insert_at_tail(&head, temp);
+        }
+
+        else if(choice==2){
+            printList(head);
+        }
+
+        else if(choice==3){
+            insert_personal_data();
+        }
+
+        else if(choice==4){
+            delete_person_data(&head);
+        }
+
+        else if(choice==5){
+            person_lookup(head);
+        }
+
+        else if(choice==6){
+            modify_person(head);
+        }
+
+        menu();
+    }
+}
 
 int main(){
-    person *head=NULL;
-    person *temp;
-
-    temp=create_person(head);
-    insert_at_tail(&head, temp);
-
-    temp=create_person(head);
-    insert_at_head(&head, temp);
-
-    temp=create_person(head);
-    insert_at_head(&head, temp);
-
-    temp=create_person(head);
-    insert_at_head(&head, temp);
-
-    temp=create_person(head);
-    insert_at_tail(&head, temp);
-
-    printList(head);
-
+    selection_function();
     return 0;
 }
