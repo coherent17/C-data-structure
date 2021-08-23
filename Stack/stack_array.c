819dde7f2a0803643cbae0aa4b9b977b71f008dc
coherent17 <mnb51817@gmail.com>
2021-08-23 22:20:34 +0800

    stack in array

---
+++ Stack/stack_array.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define STACK_LENGTH 5
+#define EMPTY (-1)
+#define INT_MIN (-2147483648)
+#define STACK_EMPTY INT_MIN
+
+int mystack[STACK_LENGTH];
+int top = EMPTY;
+
+bool push(int value){
+    if(top >= STACK_LENGTH-1)return false;
+
+    top++;
+    mystack[top] = value;
+    return true;
+}
+
+int pop(){
+    if(top==EMPTY) return STACK_EMPTY;
+
+    int reuslt = mystack[top];
+    top--;
+    return reuslt;
+}
+
+int main(){
+    push(56);
+    push(78);
+    push(13);
+
+    int t;
+    while((t=pop())!=STACK_EMPTY){
+        printf("t = %d\n", t);
+    }
+    return 0;
+}
