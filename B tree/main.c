fa7f116fd8f1006dda13321b396f96fa1de8b7aa
coherent <mnb51817@gmail.com>
2022-04-06 16:24:30 +0800

    driven code

--- B tree/main.c
+++ B tree/main.c
@@ -4,7 +4,32 @@
 #include <stdlib.h>
 #include <string.h>
 
+void printBtree(btree_node *root){
+	printf("(");
+
+	for(int i = 0; i < root->n; i++){
+		if(root->number_of_children){
+			printBtree(root->children[i]);
+		}
+		printf("%d ", root->keys[i]);
+	}
+	if(root->number_of_children){
+		printBtree(root->children[root->n]);
+	}
+
+	printf(")");
+}
+
 int main(){
-	printf("Hello World\n");
+
+	btree tree = create_btree(3);
+	int keys[7] = {8,9,10,11,15,20,17};
+	for(int i = 0; i < 7; i++){
+		btree_insert(&tree, keys[i], NULL);
+		printf("+%04d: ", keys[i]);
+		printBtree(tree.root);
+		printf("\n");
+	}
+	free_btree(&tree);
 	return 0;
 }
